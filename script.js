ae39ae82cde6209bc93c6ea9e0393e3308ff8f2e
yashsehgal29 <yashsehgal2974@gmail.com>
2024-07-31 14:43:54 +0530

    host changes

---
+++ script.js
@@ -0,0 +1,6 @@
+gsap.from(".index", {
+  y: 20,
+  opacity: 0,
+  duration: 2,
+  scale: 0.2
+});
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -94,7 +94,7 @@ const Home = () => {
         </div>
         {/* image div */}
         <div className="flex items-center justify-center w-3/4 lg:w-1/2 ">
-          <div className="flex items-center justify-center w-full ">
+          <div className="flex items-center justify-center w-full image1">
             <img
               src="https://github.com/yashsehgal29/images/blob/main/Mask%20group.png?raw=true"
               alt="Yash Sehgal"
